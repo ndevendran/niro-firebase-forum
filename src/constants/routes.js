14ab27844eebd629d6a434614c89739131ff6aa1
Niroshan Devendran <ndevendran@Niroshans-MacBook-Pro.local>
2020-08-10 17:20:46 -0700

    Adding dist and images to repo

---
+++ src/constants/routes.js
@@ -0,0 +1,10 @@
+export const LANDING = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const ADMIN = '/admin';
+export const ADMIN_DETAILS = '/admin/:id';
+export const PASSWORD_FORGET = '/account/pw-forget';
+export const PASSWORD_CHANGE = '/account/pw-change';
+export const SIGN_IN_METHODS = '/account/methods';
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',{'plugins': ['@babel/plugin-proposal-class-properties']}
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css/,
+        use: [
+          'style-loader',
+          'css-loader?modules=true'
+        ]
+      },
+      {
+        test: /\.(jpg|png|html|svg)$/,
+        use: [
+          'file-loader',
+        ]
+      },
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin(), new Dotenv()]
+};
